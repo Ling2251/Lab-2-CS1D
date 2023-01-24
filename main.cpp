753d6d16dd00471f484be66efdc9a899c21f1cbd
Peijun Zhao <pzhao1@saddleback.edu>
2023-01-23 16:22:33 -0800

    hello1Function

--- main.cpp
+++ main.cpp
@@ -26,15 +26,3 @@ int hello1Function(string name1)
  cout << "hello World " << name1 << endl; 
  return 0; 
 } 
-
- 
-/* 
- * hello2Function.cpp 
- * 
- */ 
-
-int hello2Function(string name2) 
-{ 
- cout << "hello World " << name2 << endl; 
- return 0; 
-} 
