e5969af05ca5ffd82df138725828e3aa33f74c6f
Peijun <89755404+Ling2251@users.noreply.github.com>
2023-01-24 18:03:53 -0800

    get a new branch and add hello1 Function (#4)

---
+++ function1.cpp
@@ -0,0 +1,27 @@
+// Author Peijun Zhao
+// Description: Hello World in C++ 
+//============================================================ 
+ 
+#include <iostream> 
+using namespace std; 
+
+int hello1Function(string name1); 
+int hello2Function(string name2); 
+int main() 
+{ 
+ string name1="Peijun Zhao"; 
+ hello1Function(name1); 
+ string name2="Yuan Cao"; 
+ hello2Function(name2); 
+ return 0; 
+
+
+/* 
+ * hello1Function.cpp 
+ * 
+ */ 
+int hello1Function(string name1) 
+{ 
+ cout << "hello World " << name1 << endl; 
+ return 0; 
+} 
